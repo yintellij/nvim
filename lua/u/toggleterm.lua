1a459fa8db1fae34e8e56678fba954552009303d
Salpadding <m6567fc@outlook.com>
2022-03-12 17:03:42 +0800

    height

--- lua/u/toggleterm.lua
+++ lua/u/toggleterm.lua
@@ -22,7 +22,7 @@ toggleterm.setup({
 	float_opts = {
 		border = "curved",
         width = function() return vim.o.columns - 8 end,
-        height = 40,
+        height = function() return vim.fn.winheight(0) -8 end,
 		winblend = 0,
 		highlights = {
 			border = "Normal",
